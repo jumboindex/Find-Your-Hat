b60f55f4b9a78b43405ec0da8bab94910047546a
James <ball.j.work@gmail.com>
2021-09-14 15:16:35 +0100

    finding hats...

--- main.js
+++ main.js
@@ -6,5 +6,45 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-  
+    constructor(fieldArray, x_Postion, y_Postion){
+        this.field = fieldArray;
+        this.x_Postion = x_Postion;
+        this.y_Postion = y_Postion;
+    }
+    print() {
+    for (let row in this.field){
+      console.log(this.field[row].join(''));
+    }
+    // move method:
+    // takes argument of key stroke - wasd 
+    // checks if teh move will take the player out of bound
+    // moves character w s a d 
+    // replaces previous postion with pathCharacter * 
+    // updates new postion with feildCharacter 
+    move(keystroke) {
+
+
+    }
+
+
+    // winOrLose method
+    // checks if the character has found hat or moved into hole
+   winOrLose() {
+
+   }
+
+   // play game method
+   // loops game until win or lose is true
+
+  }
 }
+
+
+
+// class creation
+ const game = new Field([['*', '░', 'O'],
+ ['░', 'O', '░'],
+ ['░', '^', '░'],]);
+
+ game.print();
+
